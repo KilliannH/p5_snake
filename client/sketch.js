2f1f983b397902c0ce156cf183947ff347b5fe25
KilliannH <killiannhervagault@gmail.com>
2023-06-28 22:22:03 +0200

    working on snake algorithm

--- client/sketch.js
+++ client/sketch.js
@@ -1,18 +1,37 @@
+let playerX = 340;
+let playerY = 180;
+
 function setup() {
     // Create the canvas
   createCanvas(720, 400);
-  background(200);
-
-  // Set colors
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
-
-  // A rectangle
-  rect(40, 120, 120, 40);
-  // An ellipse
-  ellipse(240, 240, 80, 80);
-  // A triangle
-  triangle(300, 100, 320, 100, 310, 80);
+  frameRate(20)
 }
 
-function draw() {}
+function draw() {
+  background(200);
+  fill(40, 40, 40)
+  stroke(40, 40, 40)
+  rect(playerX, playerY, 20, 20);
+
+  // works for one key pressed at time
+  if(keyIsPressed) {
+    switch (key) {
+      case "ArrowLeft":
+        console.log("left pressed");
+        playerX -=10;
+        break;
+      case "ArrowRight":
+        console.log("right pressed");
+        playerX +=10;
+        break;
+      case "ArrowUp":
+        console.log("up pressed");
+        playerY -=10;
+        break;
+      case "ArrowDown":
+        console.log("down pressed");
+        playerY +=10;
+        break;
+    }
+  }
+}
