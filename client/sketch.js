0daf0bb85890920a607075d5510380106c254d17
KilliannH <killiannhervagault@gmail.com>
2023-06-28 22:39:20 +0200

    handle movements

--- client/sketch.js
+++ client/sketch.js
@@ -1,10 +1,14 @@
 let playerX = 340;
 let playerY = 180;
+let direction = null;
+
+const speed = 5;
+const fr = 20;
 
 function setup() {
     // Create the canvas
   createCanvas(720, 400);
-  frameRate(20)
+  frameRate(fr)
 }
 
 function draw() {
@@ -12,26 +16,61 @@ function draw() {
   fill(40, 40, 40)
   stroke(40, 40, 40)
   rect(playerX, playerY, 20, 20);
-
-  // works for one key pressed at time
-  if(keyIsPressed) {
-    switch (key) {
-      case "ArrowLeft":
-        console.log("left pressed");
-        playerX -=10;
+  if(direction != null) {
+    switch (direction) {
+      case "left": {
+        if(playerX > 0) {
+          playerX -=speed;
+        } else {
+          playerX = 720;
+        }
         break;
-      case "ArrowRight":
-        console.log("right pressed");
-        playerX +=10;
+      }
+      case "right": {
+        if(playerX < 700) {
+          playerX +=speed;  
+        } else {
+          playerX = 0;
+        }
         break;
-      case "ArrowUp":
-        console.log("up pressed");
-        playerY -=10;
+      }
+      case "up": {
+        if(playerY > 0) {
+          playerY -= speed;  
+        } else {
+          playerY = 380
+        }
         break;
-      case "ArrowDown":
-        console.log("down pressed");
-        playerY +=10;
+      }
+      case "down": {
+        if(playerY < 380) {
+        playerY += speed;
+        } else {
+          playerY = 0;
+        }
         break;
+      }
     }
   }
+}
+
+function keyPressed() {
+  switch (key) {
+    case "ArrowLeft":
+      console.log("left pressed");
+      direction = "left";
+      break;
+    case "ArrowRight":
+      console.log("right pressed");
+      direction = "right";
+      break;
+    case "ArrowUp":
+      console.log("up pressed");
+      direction = "up";
+      break;
+    case "ArrowDown":
+      console.log("down pressed");
+      direction = "down";
+      break;
+    }
 }
