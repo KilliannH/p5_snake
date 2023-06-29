1b8afeadcde0979f81b50fea78c21a7b83b9b0dc
KilliannH <killiannhervagault@gmail.com>
2023-06-29 16:42:23 +0200

    fruit

--- client/sketch.js
+++ client/sketch.js
@@ -2,19 +2,35 @@ let playerX = 340;
 let playerY = 180;
 let direction = null; // left, right, up, down
 
-const speed = 5;
+let fruitX1 = randomIntFromInterval(0, 300);
+let fruitX2 = randomIntFromInterval(400, 700);
+let fruitY1 = randomIntFromInterval(200, 380);
+let fruitY2 = randomIntFromInterval(20, 160);
+
+let fruitX, fruitY;
+
+let defaultPlayerColor = {r: 40, g: 40, b: 40};
+let touchedPlayerColor = {r: 255, g: 0, b: 0}
+let playerColor = defaultPlayerColor;
+
+const speed = 3;
 const fr = 20;
 
 function setup() {
     // Create the canvas
   createCanvas(720, 400);
   frameRate(fr)
+
+  fruitX = random2values() == 0 ? fruitX1 : fruitX2;
+  fruitY = random2values() == 0 ? fruitY1 : fruitY2;
+
+  console.log(fruitX, fruitY);
 }
 
 function draw() {
   background(200);
-  fill(40, 40, 40)
-  stroke(40, 40, 40)
+  fill(playerColor.r, playerColor.g, playerColor.b)
+  stroke(playerColor.r, playerColor.g, playerColor.b)
   rect(playerX, playerY, 20, 20);
   if(direction != null) {
     switch (direction) {
@@ -52,6 +68,19 @@ function draw() {
       }
     }
   }
+
+  // draw fruit
+  fill(160, 32, 240)
+  stroke(160, 32, 240)
+  rect(fruitX, fruitY, 20, 20);
+
+
+  // handle x colider
+  if(playerX + 20 >= fruitX && playerX <= fruitX + 20 && playerY + 20 >= fruitY && playerY <= fruitY + 20) {
+    playerColor = touchedPlayerColor;
+  } else {
+    playerColor = defaultPlayerColor;
+  }
 }
 
 function keyPressed() {
@@ -69,4 +98,13 @@ function keyPressed() {
       direction = "down";
       break;
     }
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function random2values() {
+  let rand = Math.random();
+  return rand >= 0.45 ? 1 : 0;
 }
